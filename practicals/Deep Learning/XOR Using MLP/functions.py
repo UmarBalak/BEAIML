8c6036ef41d4ee65d40b32d29972ab1bbdef77c8
Umar Balak <umarbalak35@gmail.com>
2024-07-30 11:21:24 +0530

    short code

--- practicals/Deep Learning/XOR Using MLP/functions.py
+++ practicals/Deep Learning/XOR Using MLP/functions.py
@@ -1,36 +1,32 @@
 import numpy as np
 
-def unitStep(v):
+# weights
+w = np.array([1, 1]) # Weighst for AND and OR gate
+wNOT = -1
+
+# biases
+bNOT = 0.5
+bAND = -1.5
+bOR = -0.5
+
+def unitStep_activation(v):
 	if v >= 0:
 		return 1
 	else:
 		return 0
 
-def perceptronModel(x, w, b):
+def dot_activation(x, w, b):
 	v = np.dot(w, x) + b
-	y = unitStep(v)
+	y = unitStep_activation(v)
 	return y
 
-# NOT Logic Function
-# wNOT = -1, bNOT = 0.5
 def NOT_logicFunction(x):
-	wNOT = -1
-	bNOT = 0.5
-	return perceptronModel(x, wNOT, bNOT)
+	return dot_activation(x, wNOT, bNOT)
 
-# AND Logic Function
-# here w1 = wAND1 = 1, 
-# w2 = wAND2 = 1, bAND = -1.5
 def AND_logicFunction(x):
-	w = np.array([1, 1])
-	bAND = -1.5
-	return perceptronModel(x, w, bAND)
+	return dot_activation(x, w, bAND)
 
-# OR Logic Function
-# w1 = 1, w2 = 1, bOR = -0.5
 def OR_logicFunction(x):
-	w = np.array([1, 1])
-	bOR = -0.5
-	return perceptronModel(x, w, bOR)
+	return dot_activation(x, w, bOR)
 
 
--- practicals/Deep Learning/XOR Using MLP/main.py
+++ practicals/Deep Learning/XOR Using MLP/main.py
@@ -10,12 +10,7 @@ def XOR_logicFunction(x):
 	return finalOutput
 
 # testing the Perceptron Model
-test1 = np.array([0, 0])
-test2 = np.array([0, 1])
-test3 = np.array([1, 0])
-test4 = np.array([1, 1])
+test = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+for i in test:
+	print("XOR({}, {}) = {}".format(i[0], i[1], XOR_logicFunction(i)))
 
-print("XOR({}, {}) = {}".format(0, 0, XOR_logicFunction(test1)))
-print("XOR({}, {}) = {}".format(0, 1, XOR_logicFunction(test2)))
-print("XOR({}, {}) = {}".format(1, 0, XOR_logicFunction(test3)))
-print("XOR({}, {}) = {}".format(1, 1, XOR_logicFunction(test4)))
